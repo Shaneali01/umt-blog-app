af270ecb90114fe82067d79e847ac83118f206b7
SHAN-E-ALI SHAH <shanealisa456@gmail.com>
2024-09-28 16:44:50 +0500

    done change

--- backend/Routes/userroute.js
+++ backend/Routes/userroute.js
@@ -5,6 +5,6 @@ const router=express.Router();
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 router.get('/logout',logoutUser);
-router.get('/getprofile',verification,authorization,getProfile)
+router.get('/getprofile',getProfile)
 router.get('/allAdmins',GetAllAdmins)
 module.exports=router
